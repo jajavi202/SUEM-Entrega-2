f07ebec5d390dd6300a468142df78fa2f01915f6
lex8jdt <133504099+lex8jdt@users.noreply.github.com>
2025-05-20 19:39:34 +0200

    update rgb

--- source/utils/rgb.c
+++ source/utils/rgb.c
@@ -28,49 +28,10 @@ void Set_RGB(uint8_t R, uint8_t G, uint8_t B){
 	GPIO_PortSet(BOARD_LED_GREEN_GPIO_PORT, mask_set);
 }
 
-#define RGB_WHITE2()		Set_RGB(0,0,0)
-#define RGB_CYAN2()			Set_RGB(1,0,0)
-#define RGB_MAGENTA2()		Set_RGB(0,1,0)
-#define RGB_BLUE2()			Set_RGB(1,1,0)
-#define RGB_YELLOW2()		Set_RGB(0,0,1)
-#define RGB_GREEN2()		Set_RGB(1,0,1)
-#define RGB_RED2()			Set_RGB(0,1,1)
-#define RGB_BLACK2()		Set_RGB(1,1,1)
+void setGreenStatus(){
+	Set_RGB(1, 0, 1);
+}
 
-volatile extern bool stopFlag;
-
-void Test_RGB(){
-	while(!stopFlag){
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_WHITE2();
-		PRINTF("WHITE\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_CYAN2();
-		PRINTF("CYAN\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_MAGENTA2();
-		PRINTF("MAGENTA\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_BLUE2();
-		PRINTF("BLUE\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_YELLOW2();
-		PRINTF("YELLOW\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_GREEN2();
-		PRINTF("GREEN\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_RED2();
-		PRINTF("RED\r\n");
-
-		SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CoreSysClk));
-		RGB_BLACK2();
-		PRINTF("BLACK\r\n");
-	}
+void setRedStatus(){
+	Set_RGB(0, 1, 1);
 }
